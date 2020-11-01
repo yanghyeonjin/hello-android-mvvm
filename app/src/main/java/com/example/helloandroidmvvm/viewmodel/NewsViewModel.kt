acd2fc6afbab389b343e8bee02a4dcee4523f231
yanghyeonjin <guswls7081@gmail.com>
2020-11-02 01:29:58 +0900

    * 실제 API KEY 설정

--- app/src/main/java/com/example/helloandroidmvvm/viewmodel/NewsViewModel.kt
+++ app/src/main/java/com/example/helloandroidmvvm/viewmodel/NewsViewModel.kt
@@ -3,6 +3,7 @@ package com.example.helloandroidmvvm.viewmodel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.helloandroidmvvm.BuildConfig
 import com.example.helloandroidmvvm.model.NewsResponse
 import com.example.helloandroidmvvm.repository.NewsRepository
 
@@ -15,6 +16,8 @@ class NewsViewModel : ViewModel() {
 
     init {
         newsRepository = NewsRepository.getInstance()
-        _mutableLiveData = newsRepository?.getNews("google-news", "API_KEY");
+
+        // BuildConfig 클래스 사용하려면 최소 한 번 빌드 해야 함.
+        _mutableLiveData = newsRepository?.getNews("google-news", BuildConfig.NEWS_API_KEY);
     }
 }
